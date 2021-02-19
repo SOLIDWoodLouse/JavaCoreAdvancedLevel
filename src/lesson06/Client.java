69713eed2449ef67f2090b728c3434900422a7fa
SOLIDWoodLouse <solidwoodlouse@icloud.com>
2021-02-19 17:57:45 +0300

    Complete task six

---
+++ src/lesson06/Client.java
@@ -0,0 +1,50 @@
+package lesson06;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+        startTextClient();
+    }
+
+    private static void startTextClient() {
+
+        try (Socket socket = new Socket("localhost", 8180);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream());
+             Scanner sc = new Scanner(System.in)) {
+
+            String myMessage;
+
+            Thread serverReader = new Thread(() -> {
+                String serverMessage;
+                try {
+                    while (!socket.isClosed()) {
+
+                        serverMessage = in.readLine();
+                        System.out.println(serverMessage);
+
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            serverReader.start();
+
+            do {
+
+                myMessage = sc.nextLine();
+                out.println(myMessage);
+                out.flush();
+
+            } while (!myMessage.equalsIgnoreCase("stop"));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
